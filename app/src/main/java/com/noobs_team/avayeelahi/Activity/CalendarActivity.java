8a10c2fa08e8618c3b67aa8bc17aa9ceb5781ec6
smiraa <samirasayadi1995@gmail.com>
2018-10-05 21:53:24 +0330

    nothing

--- app/src/main/java/com/noobs_team/avayeelahi/Activity/CalendarActivity.java
+++ app/src/main/java/com/noobs_team/avayeelahi/Activity/CalendarActivity.java
@@ -5,11 +5,20 @@ import android.os.Bundle;
 
 import com.noobs_team.avayeelahi.R;
 
+import ir.mirrajabi.persiancalendar.PersianCalendarView;
+import ir.mirrajabi.persiancalendar.core.PersianCalendarHandler;
+import ir.mirrajabi.persiancalendar.core.models.PersianDate;
+
 public class CalendarActivity extends AppCompatActivity {
+
+    com.noobs_team.avayeelahi.Activity.Time.PersianCalendar persianCalendarView
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
+        PersianCalendarView calendarView  = (PersianCalendarView) findViewById(R.id.persian_calendar);
+        PersianCalendarHandler calendarHandler = persianCalendarView.getCalendar();
+        PersianDate today = calendar.getToday();
     }
 }
