f34e12e1deb3ec7a3d65f3e356c31f013ed23db8
smiraa <samirasayadi1995@gmail.com>
2018-11-30 02:20:13 +0330

    working on main menu

--- app/src/main/java/com/noobs_team/avayeelahi/Activity/MainActivity.java
+++ app/src/main/java/com/noobs_team/avayeelahi/Activity/MainActivity.java
@@ -36,8 +36,8 @@ public class MainActivity extends AppCompatActivity {
     android.support.v7.widget.Toolbar toolbarMain;
     Button holidays;
     ImageView calendarActivity;
-    ImageView volume1,volume2,volume3,volume4,volume5,volume6;
-    boolean b1 = true,b2 = true,b3 = true,b4 = true,b5 = true,b6 = true;
+    ImageView volume1, volume2, volume3, volume4, volume5, volume6;
+    boolean b1 = true, b2 = true, b3 = true, b4 = true, b5 = true, b6 = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,27 +65,83 @@ public class MainActivity extends AppCompatActivity {
 
     private void setupDrawer(MainActivity mainActivity, Toolbar toolbarMain) {
 
-        PrimaryDrawerItem homeItem = new PrimaryDrawerItem().withIdentifier(1).withSelectedTextColor(Color.parseColor("#000000")).withSelectedColor(Color.parseColor("#FF83CEC6")).withName(R.string.menu_home);
-        PrimaryDrawerItem asmaKhodaItem = new PrimaryDrawerItem().withIdentifier(2).withSelectedTextColor(Color.parseColor("#000000")).withSelectedColor(Color.parseColor("#FF83CEC6")).withName(R.string.menu_asma_khoda);
+        PrimaryDrawerItem homeItem = new PrimaryDrawerItem()
+                .withIdentifier(1)
+                .withSelectedTextColor(Color.parseColor("#000000"))
+                .withSelectedColor(Color.parseColor("#FF83CEC6"))
+                .withName(R.string.menu_home)
+                .withIcon(R.drawable.home);
 
-        SecondaryDrawerItem azkarRouzanehItem = new SecondaryDrawerItem().withLevel(2).withIdentifier(4).withSelectedTextColor(Color.parseColor("#000000")).withSelectedColor(Color.parseColor("#FF83CEC6")).withName(R.string.menu_azkar_rouzaneh);
-        SecondaryDrawerItem tasbihItem = new SecondaryDrawerItem().withLevel(2).withIdentifier(5).withSelectedTextColor(Color.parseColor("#000000")).withSelectedColor(Color.parseColor("#FF83CEC6")).withName(R.string.menu_tasbih);
-        SecondaryDrawerItem zekrShomarItem = new SecondaryDrawerItem().withLevel(2).withIdentifier(6).withSelectedTextColor(Color.parseColor("#000000")).withSelectedColor(Color.parseColor("#FF83CEC6")).withName(R.string.menu_zekr_shomar);
-        ExpandableDrawerItem jabehAbzarItem = new ExpandableDrawerItem().withIdentifier(3).withSelectedTextColor(Color.parseColor("#000000")).withSelectedColor(Color.parseColor("#FF83CEC6")).withName(R.string.menu_jabeh_abzar).withSubItems(
-                tasbihItem, zekrShomarItem,azkarRouzanehItem
-        );
-        PrimaryDrawerItem compassItem = new PrimaryDrawerItem().withIdentifier(7).withSelectedTextColor(Color.parseColor("#000000")).withSelectedColor(Color.parseColor("#FF83CEC6")).withName(R.string.menu_compass);
-        PrimaryDrawerItem settingsItem = new PrimaryDrawerItem().withIdentifier(8).withSelectedTextColor(Color.parseColor("#000000")).withSelectedColor(Color.parseColor("#FF83CEC6")).withName(R.string.menu_settings);
-        PrimaryDrawerItem changeDateItem = new PrimaryDrawerItem().withIdentifier(9).withSelectedTextColor(Color.parseColor("#000000")).withSelectedColor(Color.parseColor("#FF83CEC6")).withName(R.string.menu_change_date);
-        PrimaryDrawerItem aboutUsItem = new PrimaryDrawerItem().withIdentifier(10).withSelectedTextColor(Color.parseColor("#000000")).withSelectedColor(Color.parseColor("#FF83CEC6")).withName(R.string.menu_about_us);
-        PrimaryDrawerItem linkProgramItem = new PrimaryDrawerItem().withIdentifier(11).withSelectedTextColor(Color.parseColor("#000000")).withSelectedColor(Color.parseColor("#FF83CEC6")).withName(R.string.menu_send_program);
+        PrimaryDrawerItem asmaKhodaItem = new PrimaryDrawerItem()
+                .withIdentifier(2)
+                .withSelectedTextColor(Color.parseColor("#000000"))
+                .withSelectedColor(Color.parseColor("#FF83CEC6"))
+                .withName(R.string.menu_asma_khoda)
+                .withIcon(R.drawable.asma_allah);
 
+        SecondaryDrawerItem azkarRouzanehItem = new SecondaryDrawerItem()
+                .withLevel(2)
+                .withIdentifier(4)
+                .withSelectedTextColor(Color.parseColor("#000000"))
+                .withSelectedColor(Color.parseColor("#FF83CEC6"))
+                .withName(R.string.menu_azkar_rouzaneh);
+
+        SecondaryDrawerItem tasbihItem = new SecondaryDrawerItem()
+                .withLevel(2)
+                .withIdentifier(5)
+                .withSelectedTextColor(Color.parseColor("#000000"))
+                .withSelectedColor(Color.parseColor("#FF83CEC6"))
+                .withName(R.string.menu_tasbih);
+
+        SecondaryDrawerItem zekrShomarItem = new SecondaryDrawerItem()
+                .withLevel(2)
+                .withIdentifier(6)
+                .withSelectedTextColor(Color.parseColor("#000000"))
+                .withSelectedColor(Color.parseColor("#FF83CEC6"))
+                .withName(R.string.menu_zekr_shomar);
+
+        ExpandableDrawerItem jabehAbzarItem = new ExpandableDrawerItem()
+                .withIdentifier(3)
+                .withSelectedTextColor(Color.parseColor("#000000"))
+                .withSelectedColor(Color.parseColor("#FF83CEC6"))
+                .withName(R.string.menu_jabeh_abzar)
+                .withIcon(R.drawable.jabe_eslami)
+                .withSubItems(tasbihItem, zekrShomarItem, azkarRouzanehItem);
+
+        PrimaryDrawerItem compassItem = new PrimaryDrawerItem()
+                .withIdentifier(7)
+                .withSelectedTextColor(Color.parseColor("#000000"))
+                .withSelectedColor(Color.parseColor("#FF83CEC6"))
+                .withName(R.string.menu_compass);
+
+        PrimaryDrawerItem settingsItem = new PrimaryDrawerItem()
+                .withIdentifier(8)
+                .withSelectedTextColor(Color.parseColor("#000000"))
+                .withSelectedColor(Color.parseColor("#FF83CEC6"))
+                .withName(R.string.menu_settings);
+
+        PrimaryDrawerItem changeDateItem = new PrimaryDrawerItem()
+                .withIdentifier(9)
+                .withSelectedTextColor(Color.parseColor("#000000"))
+                .withSelectedColor(Color.parseColor("#FF83CEC6"))
+                .withName(R.string.menu_change_date);
+
+        PrimaryDrawerItem aboutUsItem = new PrimaryDrawerItem()
+                .withIdentifier(10)
+                .withSelectedTextColor(Color.parseColor("#000000"))
+                .withSelectedColor(Color.parseColor("#FF83CEC6"))
+                .withName(R.string.menu_about_us);
+
+        PrimaryDrawerItem linkProgramItem = new PrimaryDrawerItem()
+                .withIdentifier(11)
+                .withSelectedTextColor(Color.parseColor("#000000"))
+                .withSelectedColor(Color.parseColor("#FF83CEC6"))
+                .withName(R.string.menu_send_program);
 
         final DrawerBuilder drawerBuilder = new DrawerBuilder();
         drawerBuilder.withActivity(mainActivity);
         drawerBuilder.withToolbar(this.toolbarMain);
-        drawerBuilder.withSliderBackgroundDrawableRes(R.drawable.side_nav_bar);
-        drawerBuilder.withDrawerGravity(Gravity.END);
+        drawerBuilder.withDrawerGravity(Gravity.RIGHT);
         drawerBuilder.addDrawerItems(
 
                 homeItem,
@@ -107,26 +163,19 @@ public class MainActivity extends AppCompatActivity {
                             Intent intent = null;
                             if (drawerItem.getIdentifier() == 10) {
                                 intent = new Intent(MainActivity.this, AboutUsActivity.class);
-                            }
-                            else if (drawerItem.getIdentifier() == 7) {
+                            } else if (drawerItem.getIdentifier() == 7) {
                                 intent = new Intent(MainActivity.this, CompassActivity.class);
-                            }
-                            else if (drawerItem.getIdentifier() == 6) {
+                            } else if (drawerItem.getIdentifier() == 6) {
                                 intent = new Intent(MainActivity.this, ZekrShomarActivity.class);
-                            }
-                            else if (drawerItem.getIdentifier() == 5) {
+                            } else if (drawerItem.getIdentifier() == 5) {
                                 intent = new Intent(MainActivity.this, TasbihActivity.class);
-                            }
-                            else if (drawerItem.getIdentifier() == 11) {
-                                ///////////////////
-                            }
-                            else if (drawerItem.getIdentifier() == 9) {
+                            } else if (drawerItem.getIdentifier() == 11) {
+                                /////
+                            } else if (drawerItem.getIdentifier() == 9) {
                                 intent = new Intent(MainActivity.this, ChangeDateActivity.class);
-                            }
-                            else if (drawerItem.getIdentifier() == 2) {
+                            } else if (drawerItem.getIdentifier() == 2) {
                                 intent = new Intent(MainActivity.this, AsmaAllahActivity.class);
-                            }
-                            else if (drawerItem.getIdentifier() == 4) {
+                            } else if (drawerItem.getIdentifier() == 4) {
                                 intent = new Intent(MainActivity.this, AzkarRouzanehActivity.class);
                             }
 
@@ -178,68 +227,62 @@ public class MainActivity extends AppCompatActivity {
     private View.OnClickListener mainOnClick = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            if (v.getId() == R.id.calendar_activity){
-                startActivity(new Intent(MainActivity.this,CalendarActivity.class));
+            if (v.getId() == R.id.calendar_activity) {
+                startActivity(new Intent(MainActivity.this, CalendarActivity.class));
             }
-            if (v.getId() == R.id.holidays){
-                startActivity(new Intent(MainActivity.this,HolidaysActivity.class));
+            if (v.getId() == R.id.holidays) {
+                startActivity(new Intent(MainActivity.this, HolidaysActivity.class));
             }
-            if (v.getId() == R.id.main_volume_1){
-                if (b1){
+            if (v.getId() == R.id.main_volume_1) {
+                if (b1) {
                     volume1.setImageResource(R.drawable.silent);
                     b1 = false;
-                }
-                else if (!b1){
+                } else if (!b1) {
                     volume1.setImageResource(R.drawable.medium);
                     b1 = true;
                 }
             }
-            if (v.getId() == R.id.main_volume_2){
-                if (b2){
+            if (v.getId() == R.id.main_volume_2) {
+                if (b2) {
                     volume2.setImageResource(R.drawable.silent);
                     b2 = false;
-                }
-                else if (!b2){
+                } else if (!b2) {
                     volume2.setImageResource(R.drawable.medium);
                     b2 = true;
                 }
             }
-            if (v.getId() == R.id.main_volume_3){
-                if (b3){
+            if (v.getId() == R.id.main_volume_3) {
+                if (b3) {
                     volume3.setImageResource(R.drawable.silent);
                     b3 = false;
-                }
-                else if (!b3){
+                } else if (!b3) {
                     volume3.setImageResource(R.drawable.medium);
                     b3 = true;
                 }
             }
-            if (v.getId() == R.id.main_volume_4){
-                if (b4){
+            if (v.getId() == R.id.main_volume_4) {
+                if (b4) {
                     volume4.setImageResource(R.drawable.silent);
                     b4 = false;
-                }
-                else if (!b4){
+                } else if (!b4) {
                     volume4.setImageResource(R.drawable.medium);
                     b4 = true;
                 }
             }
-            if (v.getId() == R.id.main_volume_5){
-                if (b5){
+            if (v.getId() == R.id.main_volume_5) {
+                if (b5) {
                     volume5.setImageResource(R.drawable.silent);
                     b5 = false;
-                }
-                else if (!b5){
+                } else if (!b5) {
                     volume5.setImageResource(R.drawable.medium);
                     b5 = true;
                 }
             }
-            if (v.getId() == R.id.main_volume_6){
-                if (b6){
+            if (v.getId() == R.id.main_volume_6) {
+                if (b6) {
                     volume6.setImageResource(R.drawable.silent);
                     b6 = false;
-                }
-                else if (!b6){
+                } else if (!b6) {
                     volume6.setImageResource(R.drawable.medium);
                     b6 = true;
                 }
